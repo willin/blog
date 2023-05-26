509f6d9afd75b24d55b93d418a1c727fb4e1cf29
Willin Wang <willin@willin.org>
2023-05-26 10:24:58 +0800

    fix: policy

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const { withContentlayer } = require('next-contentlayer');
 // https://nextjs.org/docs/advanced-features/security-headers
 const ContentSecurityPolicy = `
     default-src 'self' vercel.live;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live googlesyndication.com;
     style-src 'self' 'unsafe-inline';
     img-src * blob: data:;
     media-src 'none';
