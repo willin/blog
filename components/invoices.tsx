5f9fcec573f97aa0a43ce03121171078d81af6c2
Willin Wang <willin@willin.org>
2023-05-29 15:31:57 +0800

    chore: fix style

--- components/invoices.tsx
+++ components/invoices.tsx
@@ -1,4 +1,5 @@
 'use client';
+import clsx from 'classnames';
 import useSWR from 'swr';
 import type { InvoicesTable } from '@/lib/mysql';
 import { Locale } from '@/i18n-config';
@@ -49,7 +50,13 @@ export function Invoices({ lang }: { lang: Locale }) {
 
         <div className='stat'>
           <div className='stat-title'>{t('components.balance')}</div>
-          <div className='stat-value text-primary'>{formatMoney(metrics?.in || 0 - metrics?.out || 0)}</div>
+          <div
+            className={clsx('stat-value', {
+              'text-primary': (metrics?.in || 0 - metrics?.out || 0) < 0,
+              'text-secondary': (metrics?.in || 0 - metrics?.out || 0) >= 0
+            })}>
+            {formatMoney(metrics?.in || 0 - metrics?.out || 0)}
+          </div>
           <div className='stat-desc'>
             {t('components.this_year')} {formatMoney(metrics?.yearIn || 0 - metrics?.yearOut || 0)}
           </div>
