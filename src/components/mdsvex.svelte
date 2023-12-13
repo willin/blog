edfac1257fb7639ca528fd28ecb335ea8579facf
Willin Wang <i@v0.md>
2023-12-13 15:41:44 +0800

    feat: :tada: init

---
+++ src/components/mdsvex.svelte
@@ -0,0 +1,52 @@
+<!-- <script lang="ts">
+  import { createTableOfContents } from '@melt-ui/svelte';
+  import Toc from './toc.svelte';
+  import AdSlot from './AdSlot.svelte';
+  import SEO from './SEO.svelte';
+
+  const { title, desc } = $props<{ title: string; desc: string }>();
+
+  const {
+    elements: { item },
+    states: { activeHeadingIdxs, headingsTree }
+  } = createTableOfContents({
+    activeType: 'highest-parents',
+    selector: '#article'
+  });
+</script>
+
+{#if title}
+  <SEO {title} {desc} />
+{/if}
+
+<div
+  class="hero min-h-screen bg-neutral"
+  style="background-image: url(https://willin.wang/images/bg.jpg);">
+  <div class="hero-overlay bg-opacity-60"></div>
+  <div class="hero-content text-center">
+    <div class="max-w-full">
+      {#if title}
+        <h1 class="text:2xl lg:text-5xl font-bold glass rounded-lg p-4">
+          {@html title}
+        </h1>
+      {/if}
+      {#if desc}
+        <p class="py-6">{@html desc}</p>
+      {/if}
+    </div>
+  </div>
+</div>
+
+<div class="grid grid-cols-3">
+  <article
+    id="article"
+    class="col-span-3 lg:col-span-2 prose prose-sm md:prose-base w-full !max-w-full p-2 lg:p-10">
+    <AdSlot />
+    <slot />
+  </article>
+  <aside class="hidden lg:block p-10">
+    {#key $headingsTree}
+      <Toc tree={$headingsTree} activeHeadingIdxs={$activeHeadingIdxs} {item} />
+    {/key}
+  </aside>
+</div> -->
---
+++ svelte.config.js
@@ -0,0 +1,41 @@
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  extensions: ['.svelte', ...mdsvexConfig.extensions],
+  preprocess: [mdsvex(mdsvexConfig), vitePreprocess()],
+  // compilerOptions: {
+  //   runes: true
+  // },
+  kit: {
+    alias: {
+      $components: './src/components'
+    },
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: true,
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      }
+    })
+  },
+  onwarn: (warning, handler) => {
+    if (warning.code.startsWith('a11y-no-noninteractive-tabindex')) {
+      return;
+    }
+    handler(warning);
+  }
+};
+
+export default config;
