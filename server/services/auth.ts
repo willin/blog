b9bb6c33aee028d495db3bce2e76def4450ba2bd
Willin Wang <i@v0.md>
2024-02-12 02:57:57 +0800

    docs: :memo: about

--- server/services/auth.ts
+++ server/services/auth.ts
@@ -50,9 +50,7 @@ export class AuthService implements IAuthService {
       throwOnError: true
     });
 
-    const callbackURL = `${url.protocol}//${url.hostname}${
-      ['80', '443', ''].includes(url.port) ? '' : `:${url.port}`
-    }/auth/sso/callback`;
+    const callbackURL = `${url.origin}/auth/sso/callback`;
     this.#authenticator.use(
       new SSOStrategy(
         {
